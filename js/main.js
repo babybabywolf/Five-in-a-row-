8e568ed430d69cc3b88caa21b5f3bbf09c4bdd4a
Amber <windjiayu@gmail.com>
2023-12-09 11:55:15 +1100

    set up starter code

---
+++ js/main.js
@@ -0,0 +1,38 @@
+//constants
+
+
+
+//state variable
+
+let board;
+let currentPlayer;
+let winner;
+
+/*----- event listeners -----*/
+//event listener for on click of the intersection to place a piece:
+ //check for valid moves (if intersection is empty)
+ //check for win
+ //update the game board
+//toggle currentplayer
+
+//event listener for on clikc of play again to reset or start a new game
+/*----- functions -----*/
+
+function defaultBoard(){}
+
+function checkWin(){
+
+}
+
+function initialise(){
+
+}
+
+function render(){}
+
+function renderBoard(){}
+
+//update the game board based on player moves
+
+function renderMessage(){}
+//show game messages (turn, result)
